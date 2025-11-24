f9c38c10ca0341a0076162af026a8baf64796682
jcasib <jcasib01@gmail.com>
2025-11-24 00:56:36 +0000

    Retoques

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+let contador = {
+  ganadas: 0,
+  perdidas: 0,
+  empates: 0,
+};
+
 
 const jugar = (userChoice) => {
   // Opciones del juego
@@ -19,11 +25,21 @@ const jugar = (userChoice) => {
   //Equiparación del resultado
   if (computerChoice === userChoice) {
     resultado = `Empate, ambos habéis elegido ${userChoice}`;
+    contador.empates += 1;
   } else if (rules[computerChoice][0] === userChoice || rules[computerChoice][1] === userChoice) {
     resultado = `Has perdido, ${computerChoice} mata a ${userChoice}`;
+    contador.perdidas += 1;
   } else {
     resultado = `Has ganado, ${userChoice} mata a ${computerChoice}`;
+    contador.ganadas += 1;
   }
-  
+
   document.getElementById("resultado").textContent = resultado;
+
+
+  document.getElementById("ganadas").textContent = contador.ganadas;
+  document.getElementById("perdidas").textContent = contador.perdidas;
+  document.getElementById("empates").textContent = contador.empates;
+
+
 }
